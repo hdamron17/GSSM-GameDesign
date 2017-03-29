736459831c31f804b9095932840eb6c2399ac16c
roboticsteam <jjohnson18@gssm.k12.sc.us>
2017-03-29 19:21:24 -0400

    Preparing to start work

--- src/tiger.py
+++ src/tiger.py
@@ -10,8 +10,6 @@ import copy
 import math
 
 
-#TODO Make sure tigers cannot go on top of each other (currently possible)
-
 def new_row(nodesY, prevX, layer, masterX=0.5, debug=False):
     '''
     Constructs new row for drawing board
@@ -395,26 +393,3 @@ if __name__ == "__main__":
             pointers=([first_node] if first_node != None else []))
 
         pygame.display.flip()
-
-
-while not done and False: ##TODO remove this (here for reference only)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            is_blue = not is_blue
-
-    pressed = pygame.key.get_pressed()
-    if pressed[pygame.K_UP]: y -= 3
-    if pressed[pygame.K_DOWN]: y += 3
-    if pressed[pygame.K_LEFT]: x -= 3
-    if pressed[pygame.K_RIGHT]: x += 3
-
-    if is_blue: color = (0, 128, 255)
-    else: color = (255, 100, 0)
-
-    screen.fill((0, 0, 0))
-    pygame.draw.rect(screen, color, pygame.Rect(x, y, 60, 60))
-
-    pygame.display.flip()
-    clock.tick(60)
